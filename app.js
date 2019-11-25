523beef4857140ceabdd097545948175b4846ecd
equeslevi <rjustineduardo@yahoo.com>
2019-11-25 16:05:41 +0800

    reduced unneccesary data

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ const hbs = exphbs.create({
 	extname: '.jets'
 });
 const port = process.env.PORT || 1108;
+const version = "1.0.0.1";
 
 //db start
 // const couch = new NodeCouchDB({
@@ -45,14 +46,12 @@ function timeStamp(color, text){
 	console.log(`${tynt.Yellow(moment().format("dddd, MMMM Do YYYY, h:mm:ss a"))}` + y);
 };
 
-// app.get('/favicon.ico', (req, res) => res.status(204));
-// app.get('/css/style/css', (req, res) => res.status(204));
-
 //
 app.use(express.static(path.join(__dirname, '/public')));
 
 // Init middleware
 app.use('/', logger);
+
 
 // Handlebars Middleware
 app.engine('.jets', hbs.engine);
@@ -62,270 +61,35 @@ app.set('view engine', '.jets');
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 
-
+app.get('/favicon.ico', (req, res) => res.status(204));
 //homepage route
 app.get('/', function(req,res){
 	res.render('index', { 
 		alertMsg: undefined,
 		title: 'T A W M Y D',
 		titleurl: '/',
-		navtitle: 'JET Services',
+		navtitle: 'TAWMYD',
 		link1: 1,
 		link1url: '/',
   		link1name: 'Home',
-  		link2: 1,
-		link2url: '/jregistration',
-		link2name: 'JRegistration',
-		link3: 1,
-		link3url: '/golev',
-		link3name: 'GoLev SKU',
-		jets: true,
-		allowPortrait: true,
-	});
-});
-
-//jregistration Route
-app.get('/jregistration', function(req,res){
-	res.render('jregistrationPage', { 
-  		alertMsg: undefined,
-  		title: 'JRegistration Homepage',
-  		titleurl: '/jregistration',
-  		navtitle: 'JRegistration',
-  		link1: 1,
-  		link1url: '/jregistration',
-  		link1name: 'Home',
-  		link2: 1,
-		link2url: '/jregistration/members',
-		link2name: 'Members',
-		link3: 1,
-		link3url: '/jregistration/members/new',
-		link3name: 'New',
-		link8: 1,
-		link8url: '/',
+  		link8: 1,
+		link8url: '/jetservices',
 		link8name: 'JET Services'
 	});
 });
 
-app.all('/jregistration/members', function(req,res){
-  couch.get(dbName, viewUrl).then(
-  	function(data, headers, status){
-  		let text='Members data rendered';
-  		timeStamp('Green', text);
-  		res.render('membersPage', {
-  			alertMsg: undefined || req.body.redirectMsg,
-  			alerttype: undefined || req.body.redirecttype,
-  			title: 'Members Page',
-  			titleurl: '/jregistration',
-  			navtitle: 'JRegistration',
-  			link1: 1,
-  			link1url: '/jregistration',
-  			link1name: 'Home',
-  			link2: 1,
-			link2url: '/jregistration/members',
-			link2name: 'Members',
-			link3: 1,
-			link3url: '/jregistration/members/new',
-			link3name: 'New',
-			link8: 1,
-			link8url: '/',
-			link8name: 'JET Services',
-		    members: data.data.rows
-		  });
-  	},
-  	function(err){
-  		let text='Cannot connect to database, now using demo-data. Actions are limited, options are disabled';
-  		timeStamp('Red', text);
-  		timeStamp('Red', err);
-  		fs.readFile('./files/demo.json', 'utf8', function(err2, data) {
-			if (err2) throw err2;
-			let members = (data);
-			res.render('membersPage', { 
-		  		alertMsg: text,
-		  		title: 'JRegistration Homepage',
-		  		titleurl: '/jregistration',
-		  		navtitle: 'JRegistration',
-		  		link1: 1,
-		  		link1url: '/jregistration',
-		  		link1name: 'Home',
-		  		link2: 1,
-				link2url: '/jregistration/members',
-				link2name: 'Members',
-				link3: 1,
-				link3url: '/jregistration/members/new',
-				link3name: 'New',
-				link8: 1,
-				link8url: '/',
-				link8name: 'JET Services',
-				demomembers: members
-  		});
-		});
-
-  	});
-});
-
-app.all('/jregistration/members/new', function(req,res){
-	res.render('newmemberPage', { 
-  		alertMsg: undefined  || req.body.redirectMsg,
-  		title: 'Create New',
-  		alerttype: 'success',
-  		titleurl: '/jregistration',
-  		navtitle: 'JRegistration',
-  		link1: 1,
-  		link1url: '/jregistration',
-  		link1name: 'Home',
-  		link2: 1,
-		link2url: '/jregistration/members',
-		link2name: 'Members',
-		link3: 1,
-		link3url: '/jregistration/members/new',
-		link3name: 'New',
-		link8: 1,
-		link8url: '/',
-		link8name: 'JET Services',
-		validator: true
+//jetservices route
+app.get('/jetservices', function(req,res){
+	res.render('jetservices', { 
+		alertMsg: undefined,
+		title: 'JET Services',
+		titleurl: '/jetservices',
+		navtitle: 'JET Services',
+		link1: 1,
+		link1url: '/',
+  		link1name: 'TAWMYD',
+  		jets: true
 	});
-});
-
-app.post('/jregistration/members/add/new', function(req,res){
-	const first = req.body.first;
-	const last = req.body.last;
-	const city = req.body.city;
-	const age = req.body.age;
-	const dob = req.body.dob;
-	const com = req.body.com;
-	const contact = req.body.contact;
-	const email = req.body.email;
-	const standing = "good";
-	const lastpayment = "";
-	const lastpayamt = "";
-	const dues = "0";
-
-	couch.uniqid().then(function(ids){
-		const id = ids[0];
-
-		couch.insert(dbName, {
-			_id: id,
-			first: first,
-			last: last,
-			city: city,
-			age: age,
-			dob: dob,
-			com: com,
-			contact: contact,
-			email: email,
-			standing: standing,
-			lastpayment: lastpayment,
-			lastpayamt: lastpayamt,
-			dues: dues
-		}).then(
-			function(data, headers, status){
-		  		let text= first +' '+last+' successfully added';
-		  		timeStamp('Green', text);
-				res.render('rerouter', {
-					alertMsg: text,
-					rerouter: true,
-					routerUrl: '/jregistration/members/new',
-				});
-			},
-			function(err){
-				res.send(err);
-		});
-	});
-});
-
-app.post('/jregistration/member/edit/', function(req, res){
-	const id = req.body.ekey;
-	const rev = req.body.erev;
-	const first = req.body.first;
-	const last = req.body.last;
-	const city = req.body.city;
-	const age = req.body.age;
-	const dob = req.body.dob;
-	const com = req.body.com;
-	const contact = req.body.contact;
-	const email = req.body.email;
-	const standing = req.body.standing;
-	const lastpayment = req.body.lastpayment;
-	const lastpayamt = req.body.lastpayamt;
-	const dues = req.body.dues;
-	console.log(timeStamp('Red', id))
-	console.log(timeStamp('Red', rev))
-	couch.update(dbName, {
-		_id: id,
-		_rev: rev,
-		first: first,
-		last: last,
-		city: city,
-		age: age,
-		dob: dob,
-		com: com,
-		contact: contact,
-		email: email,
-		standing: standing,
-		lastpayment: lastpayment,
-		lastpayamt: lastpayamt,
-		dues: dues
-		}).then(
-		function(data, headers, status){
-			let text= first +' '+ last +' successfully updated';
-		  	timeStamp('Green', text);
-		  	res.render('rerouter', {
-				alertMsg: text,
-				alerttype: 'success',
-				rerouter: true,
-				routerUrl: '/jregistration/members',
-				title: 'Loading...',
-			  	});
-			},
-			function(err){
-				console.log(err);
-				res.redirect('/jregistration/members');
-		});
-});
-
-app.post('/jregistration/member/delete/', function(req, res){
-	const id = req.body.delKey;
-	const rev = req.body.delRev;
-	const name = req.body.delName;
-	console.log(timeStamp('Red', id))
-	console.log(timeStamp('Red', rev))
-	couch.del(dbName, id, rev).then(
-		function(data, headers, status){
-			let text= name +' removed successfully';
-		  	timeStamp('Red', text);
-		  	res.render('rerouter', {
-				alertMsg: text,
-				rerouter: true,
-				routerUrl: '/jregistration/members',
-				title: 'Loading...'
-			  	});
-			},
-			function(err){
-				console.log(err);
-				res.redirect('/jregistration/members');
-		});
-});
-
-app.get('/golev', function(req,res){
-	fs.readFile('./files/items.json', 'utf8', function(err, data) {
-		if (err) throw err;
-		let items =  (data);
-		res.render('golevPage', { 
-	  		alertMsg: undefined,
-	  		title: 'GoLev SKU',
-	  		titleurl: '/golev',
-	  		navtitle: 'GoLev SKU',
-	  		link1: 1,
-	  		link1url: '/golev',
-	  		link1name: 'Home',
-			link8: 1,
-			link8url: '/',
-			link8name: 'JET Services',
-			golev: true,
-			items: items
-		});
-	});
-
 });
 
 function serverStart() {
@@ -342,7 +106,8 @@ function serverStart() {
 		console.log(`${tynt.Yellow(' /        \\  ___/|  | \\/\\   /\\  ___/|  | \\/  |    |   \\  |  /   |  \\   |  \\  |   |  \\/ /_/  >')}`);
 		console.log(`${tynt.Yellow('/_______  /\\___  >__|    \\_/  \\___  >__|     |____|_  /____/|___|  /___|  /__|___|  /\\___  / ')}`);
 		console.log(`${tynt.Yellow('        \\/     \\/                 \\/                \\/           \\/     \\/        \\//_____/  ')}`);
-		console.log(`${tynt.Yellow(' =================================Server running on '+ port +' ===================================')}`);
+		console.log(`${tynt.Yellow(' ==================================== TAWMYD v. '+ version +' ====================================')}`);
+		console.log(`${tynt.Yellow(' ================================ Server running on '+ port +' ===================================')}`);
 	}); 
 };
 
--- public/js/main.js
+++ public/js/main.js
@@ -9,140 +9,27 @@ let rerouter = '{{rerouter}}';
 
 
 
-$('form.delForm').on('submit', function() {
-	$('#moddel').modal('toggle');
-	$("#delForm").submit();
-    return false
-});
+// viewportchecker
+// function checkViewPort() {
+// 	if (mustbeLandscape===true) {
+// 		if(window.innerHeight > window.innerWidth){
+// 	        $('#portaitalert').removeClass('d-none');
+// 	        $('#content').addClass('d-none');
+// 	    } else {
+// 	    	$('#portaitalert').addClass('d-none');
+// 	    	$('#content').removeClass('d-none');
+// 	    }
+// 	} else {
+// 		$('#portaitalert').addClass('d-none');
+// 	    $('#content').removeClass('d-none');
+// 	}
+// };
 
-$('form.editForm').on('submit', function() {
-	// $('#modup').modal('toggle');
-	$("#updateSendBtn").attr("disabled", true);
-	$("#updateSendBtn").text('Adding...');
-	$("#editForm").submit();
-	return false
-});
-
-// NEW MEMBER PAGE
-
-
-function checkViewPort() {
-	if (mustbeLandscape===true) {
-		if(window.innerHeight > window.innerWidth){
-	        $('#portaitalert').removeClass('d-none');
-	        $('#content').addClass('d-none');
-	    } else {
-	    	$('#portaitalert').addClass('d-none');
-	    	$('#content').removeClass('d-none');
-	    }
-	} else {
-		$('#portaitalert').addClass('d-none');
-	    $('#content').removeClass('d-none');
-	}
-};
-
-$(window).resize(function(){
-    checkViewPort();
-});
+// $(window).resize(function(){
+//     checkViewPort();
+// });
 
 $(document).ready(function () {
-	//for rerouter
-	$("#rerouteForm").submit();
-	//for rerouter end
-	if (demo===false) {
-		$('#membersTable').DataTable();
-		$('.dataTables_length').addClass('bs-select');
-	}
-	checkViewPort();
-    $('#portaitalert').removeClass('invisible');
-    $('#content').removeClass('invisible');
-    
-    //demo members load
-    if (demo===true) {
-    	let tablerows
-	    for (var i = member.length - 1; i >= 0; i--) {
-	    	let row
-	    	row = row+"<tr id='row"+member[i].key+"'><td class='pl-1'>"+member[i].last+"</td><td class='pl-1'>"+member[i].first+"</td><td class='pl-1'>"+member[i].age+"</td><td class='pl-1'>"+member[i].city+"</td><td class='pl-1'>"+member[i].com+"</td>";
-	        row = row+"<td><div class='text-center'><button type='button' class='btn btn-info editBtn' id='e"+member[i].key+"' data-toggle='modal' data-target='#modup'><span class='text-white'>✏</span></button>"
-	        row = row+"<button type='button' class='btn btn-info printBtn' id='p"+member[i].key+"' data-toggle='modal' data-target='#modprint'><span class='text-white'>🖨️</span></button>"
-	        row = row+"<button type='button' class='btn btn-danger delBtn' id='d"+member[i].key+"' data-toggle='modal' data-target='#moddel'><span class='text-white' aria-hidden='true'>✖️</span></button></div></td></tr>"
-	    	tablerows = tablerows + row;
-	    }
-	    $("table tbody").append(tablerows);
-	    $('#membersTable').DataTable();
-		$('.dataTables_length').addClass('bs-select');
-		$("#updateSendBtn").attr("disabled", true);
-		$("#printSendBtn").attr("disabled", true);
-		$("#delSendBtn").attr("disabled", true);
-    }
-    
-    $('.closebtn').click(function() {
-		let div = this.parentElement;
-		$(div).addClass('d-none');
-	});
-
-	$(".editBtn").click(function() {
-	    let x = this.id;
-	    let id = x.substring(1, x.length);
-	    console.log(id);
-	    $("#upKey").val(id);
-		let name;
-	    for (var i = member.length - 1; i >= 0; i--) {
-		    if (member[i].key === id) {
-		        console.log('Pulling up  ' + member[i].first +' '+ member[i].last +"'s data");
-		        name = member[i].first +' '+ member[i].last;
-		    	$('#editHeader').text(name);
-		    	$("#upRev").val(member[i].rev);
-		    	$('#inputeFirst').val(member[i].first);
-				$('#inputeLast').val(member[i].last);
-				$('#inputeAge').val(member[i].age);
-				$('#inputeDob').val(member[i].dob);
-				$('#inputeEmail').val(member[i].email);
-				$('#inputeContact').val(member[i].contact);
-				$('#inputeCompany').val(member[i].com);
-				$('#inputeCity').val(member[i].city);
-				$('#inputeLastpayamt').val(member[i].lastpayamt);
-				$('#inputeLastpayment').val(member[i].lastpayment);
-				$('#inputeStanding').val(member[i].standing);
-				$('#inputeDues').val(member[i].dues);
-		    };
-	  	};
-	  	console.log('rev# ' + $("#upRev").val());
-	});
-
-	$(".printBtn").click(function() {
-	    let x = this.id;
-	    let id = x.substring(1, x.length);
-	    console.log(id);
-		let name;
-	    for (var i = member.length - 1; i >= 0; i--) {
-		    if (member[i].key === id) {
-		        console.log(member[i].first +' '+ member[i].last);
-		    	name = member[i].first +' '+ member[i].last;
-		    };
-	  	};
-		$("#printHeader").text(name);
-	});
-
-	$(".delBtn").click(function() {
-	    let x = this.id;
-	    let id = x.substring(1, x.length);
-	    console.log(id);
-	    $("#delKey").val(id);
-		let name;
-		let rev;
-	    for (var i = member.length - 1; i >= 0; i--) {
-		    if (member[i].key === id) {
-		        console.log(member[i].first +' '+ member[i].last);
-		    	name = member[i].first +' '+ member[i].last;
-		    	rev = member[i].rev;
-		    };
-	  	};
-	  	console.log(rev);
-	  	$("#delRev").val(rev);
-	  	$("#delName").val(name);
-		$("#delHeader").text(name);
-	});
-
+	
 });
 
